a65b24897d8f95652be7a8af450a989497fca427
swissminion06 <81765664+swissminion06@users.noreply.github.com>
2024-11-01 01:23:55 +0100

    change text and add image

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,26 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('Läckbobi!'),
-            Text('De Server esch chorz chli z steil gange. Omloh holt chorz es taktischs ZwiWa.'),
-          ],
-        ) 
+        body: Center( // Wrap the Column in Center
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('images/omloh_maintenance_page.png')
+              ),
+              Text('Läckbobi!', style: TextStyle(fontSize: 30),),
+              Text('omloh.ch esch grad ide Wartig!', style: TextStyle(fontSize: 20),),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0), // Add horizontal padding for wrapping
+                child: Text(
+                  'Lueg doch spöter weder verbii, omloh.ch esch grad weder online.',
+                  textAlign: TextAlign.center, // Center-align the text within the container
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
